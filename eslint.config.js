14f97ad9f963d9594d50682ae1aeaa47b8f7037c
levisantosp <contatowizard01@gmail.com>
2025-09-03 21:59:55 -0300

    switch to double quotes

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import { defineConfig } from "eslint/config"
 
 export default defineConfig([
   {
     ignores: [
-      '*test*',
-      'dist'
+      "*test*",
+      "dist"
     ]
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
-    extends: ['js/recommended']
+    extends: ["js/recommended"]
   },
   { 
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     languageOptions: { globals: globals.browser }
   },
   tseslint.configs.recommended,
   {
     rules: {
-      indent: ['error', 2, {
-        ignoredNodes: ['CallExpression > MemberExpression']
+      indent: ["error", 2, {
+        ignoredNodes: ["CallExpression > MemberExpression"]
       }],
-      quotes: ['error', 'single'],
-      'keyword-spacing': ['error', {
+      quotes: ["error", "double"],
+      "keyword-spacing": ["error", {
         before: true,
         after: true,
         overrides: {
@@ -37,13 +37,13 @@ export default defineConfig([
           catch: { after: false }
         }
       }],
-      'space-before-function-paren': ['error', {
-        anonymous: 'never',
-        named: 'never',
-        asyncArrow: 'never'
+      "space-before-function-paren": ["error", {
+        anonymous: "never",
+        named: "never",
+        asyncArrow: "never"
       }],
-      '@typescript-eslint/no-explicit-any': 'off',
-      'no-empty': 'off'
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-empty": "off"
     }
   }
 ])
