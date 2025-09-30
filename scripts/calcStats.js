348f89278f141ce40b48deb5bcee12a8a0034da7
levisantosp <contatowizard01@gmail.com>
2025-09-30 13:27:04 -0300

    minor changes

--- scripts/calcStats.js
+++ scripts/calcStats.js
@@ -12,13 +12,17 @@ export default function(stats) {
   const ACS = Number(attr[attr.findIndex(a => a === 'ACS') + 1])
   const APR = Number(attr[attr.findIndex(a => a === 'APR') + 1])
   const FBSR = Number(attr[attr.findIndex(a => a === 'FBSR') + 1].replace('%', '').trim())
+
   const aim = (KPR * 40) + (HS * 1.8)
   const hs = HS * 2.6
   const mov = (1.5 - DPR) * 95
   const aggression = (FBPR * 300) + (KPR * 30)
   const acs = ACS / 3
   const gamesense = (APR * 150) + (FBSR * 0.6)
+
   const attributes = `,${aim.toFixed(2)},${hs.toFixed(2)},${mov.toFixed(2)},${aggression.toFixed(2)},${acs.toFixed(2)},${gamesense.toFixed(2)},true`
+
   clipboard.writeSync(attributes)
+  
   return `copied to clipboard\n,${aim.toFixed(2)},${hs.toFixed(2)},${mov.toFixed(2)},${aggression.toFixed(2)},${acs.toFixed(2)},${gamesense.toFixed(2)},true`
 }
